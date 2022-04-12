4dda5af5831b02163b4e735d60ca601cad4c0fce
Mateusz Fortuna <mat.fortuna@wp.pl>
2022-04-12 08:50:24 +0200

    Add project files.

---
+++ zaliczenie2/LinearSieve.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace zaliczenie2
+{
+    class LinearSieve
+    {
+        private void remove()
+        {
+
+        }
+        private void next()
+        {
+
+        }
+
+        public static int[] getResult(int[] numbers)
+        {
+            if (numbers[0] < 2)
+                throw new ArgumentException("The first integer of the array cannot be smaller than two.");
+
+            if (numbers[1] <= numbers[0] || numbers[1] - numbers[0] != 1)
+                throw new ArgumentException("The list of integers is not consecutive.");
+
+            int[] result;
+            int p = numbers[0];
+            int q;
+            int x;
+
+            while (p * p <= numbers[^1])
+            {
+                q = p;
+
+                while (p * q <= numbers[^1])
+                {
+                    x = p * q;
+                }
+            }
+
+            return result;
+        }
+    }
+}
